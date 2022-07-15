35c82babb1fd66fed1f20fc7687428a153b8097c
Abhishek Kumar <95921032+abhishekkumarXD@users.noreply.github.com>
2022-07-15 22:15:31 +0530

    Add "domain" Package
    "domain" package contains Order.java file

---
+++ src/com/acmeordersystem/domain/Order.java
@@ -0,0 +1,49 @@
+package com.acmeordersystem.domain;
+
+import com.acmeordersystem.utils.MyDate;
+
+public class Order {
+	MyDate orderDate;
+	double orderAmount = 0.00;
+	String customer;
+	String product;
+	int quantity;
+	// static initialization block
+	public static double taxRate = 0.05;
+
+	// sets a new tax rate
+	public static void setTaxRate(double newRate) {
+		taxRate = newRate;
+	}
+
+	// compute tax on an amount
+	public static void computeTaxOn(double anAmount) {
+		System.out.println("The tax for " + anAmount + " is: " + anAmount * Order.taxRate);
+	}
+
+	// compute tax for an order object
+	public double computeTax() {
+		System.out.println("The tax for this order is: " + orderAmount * Order.taxRate);
+		return orderAmount * Order.taxRate;
+	}
+
+	public Order(MyDate d, double amt, String c, String p, int q){
+		orderDate=d;
+		orderAmount=amt;
+		customer=c;
+		product=p;
+		quantity=q;
+	}
+
+	public Order(MyDate d, double amt, String c) {
+		orderDate = d;
+		orderAmount = amt;
+		customer = c;
+		product = "Anvil";
+		quantity = 1;
+	}
+
+	public String toString(){
+		return quantity + " ea. " + product + " for " + customer;
+	}
+}
