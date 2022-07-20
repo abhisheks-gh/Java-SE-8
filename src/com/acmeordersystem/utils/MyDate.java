938d9ac3d13c6c23a441926f1ae2a80862d992d4
Abhishek Kumar <95921032+abhishekkumarXD@users.noreply.github.com>
2022-07-20 21:06:05 +0530

    Update "leapYears()" to check leap years.
    Update "leapYears()" to check leap years between the years 1752 and 2020.

--- src/com/acmeordersystem/utils/MyDate.java
+++ src/com/acmeordersystem/utils/MyDate.java
@@ -9,7 +9,7 @@ package com.acmeordersystem.utils;
 public class MyDate {
     public int day;
     public int month;
-    public int year;
+    public static int year;
     // No-args constructor so that the default constructor of the class can be used.
     public MyDate() {}
     // Constructor
@@ -30,4 +30,26 @@ public class MyDate {
         day = d;
         year = y;
     }
+
+    /** Checks which years are leap years */
+    public static void leapYears() {
+        /**
+        Some simple calculators use a basic
+        loop. Other more accurate calculators use a loop and modulus operator,
+        shown in the formula below.
+         Every year that is divisible by four is a leap year.
+         Of those years, if it can be divided by 100, it is NOT a leap year,
+           unless the year is divisible by 400. Then it is a leap year.
+         */
+
+        for (year = 1752; year <= 2020; year++) {
+            // Leap years
+            if (year % 4 == 0 && year % 100 != 0)
+                System.out.println("The year " + year + " is a leap year");
+            else if (year % 4 == 0 && year % 100 == 0 && year % 400 == 0) {
+                System.out.println("The year " + year + " is a leap year");
+            }
+        }
+    }
+
 }
