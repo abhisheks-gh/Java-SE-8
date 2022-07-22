f7c0644541b8978d149ae3308e2889ad8f01cba1
Abhishek Kumar <95921032+abhishekkumarXD@users.noreply.github.com>
2022-07-23 01:22:01 +0530

    Update TestMyDate.java

--- src/com/acmeordersystem/testing/TestMyDate.java
+++ src/com/acmeordersystem/testing/TestMyDate.java
@@ -4,13 +4,13 @@ import com.acmeordersystem.utils.MyDate;
 
 public class TestMyDate{
 
-	public static void main(String[] args){
+	public static void main(String[] args) {
 		MyDate date1 = new MyDate(11,11,1918);
 
 		MyDate date2 = new MyDate();
-		date2.day = 11;
-		date2.month = 11;
-		date2.year = 1918;
+		date2.setDay(11);
+		date2.setMonth(11);
+		date2.setYear(1918);
 
 		MyDate date3 = new MyDate();
 		date3.setDate(4,21,1968);
@@ -26,4 +26,5 @@ public class TestMyDate{
 		// tests the method leapYears()
 		// MyDate.leapYears();
 	}
+
 }
