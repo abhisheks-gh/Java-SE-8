0cc6f2636b1396e5c62223c9580fe973f1939ca0
Abhishek Kumar <95921032+abhishekkumarXD@users.noreply.github.com>
2022-07-23 01:20:55 +0530

    Update PassByExperiment.java

--- src/com/acmeordersystem/testing/PassByExperiment.java
+++ src/com/acmeordersystem/testing/PassByExperiment.java
@@ -6,15 +6,15 @@ public class PassByExperiment {
 
     public static void main(String[] args) {
         // Created MyDate object
-        MyDate date  = new MyDate(1,20,2008);
+        MyDate date = new MyDate(1, 20, 2008);
 
         System.out.println("Before passing an object " + date); // 1/20/2008
         passObject(date);
         System.out.println("After passing an object " + date);  // 1/20/2009
 
-        System.out.println("Before passing a primitive " + MyDate.year);    // 2009
-        passPrimitive(MyDate.year);
-        System.out.println("After passing a primitive " + MyDate.year); // 2009
+        System.out.println("Before passing a primitive " + date.getYear());    // 2009
+        passPrimitive(date.getYear());
+        System.out.println("After passing a primitive " + date.getYear()); // 2009
 
         String x = date.toString();
         System.out.println("Before passing a String " + x); // 1/20/2009
@@ -22,20 +22,22 @@ public class PassByExperiment {
         System.out.println("After passing a String " + x);  // 1/20/2010
     }
 
-    /** Changes the year of the MyDate object */
-    public static void passObject(MyDate d) {
-        d.year = 2009;
-    }
+    /**
+     * Changes the year of the MyDate object
+     */
+    public static void passObject(MyDate d) { d.setYear(2009); }
 
-
-    /** Changes the int to some other year */
+    /**
+     * Changes the int to some other year
+     */
     public static void passPrimitive(int i) {
         i = 2010;
     }
 
-    /** passes a String that represents a month/day/year and replaces the year
-     portion of the string with a new year. For example, if “1/20/2001” is
-     passed in, “1/20/2012” is passed out.
+    /**
+     * passes a String that represents a month/day/year and replaces the year
+     * portion of the string with a new year. For example, if “1/20/2001” is
+     * passed in, “1/20/2012” is passed out.
      */
     public static void passString(String s) {
         int yearSlash = s.lastIndexOf('/');
