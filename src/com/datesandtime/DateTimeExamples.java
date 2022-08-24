08b3e3e13b4590b47003194d105e6a99afc22a69
Abhishek Kumar <95921032+abhishekkumarXD@users.noreply.github.com>
2022-08-25 00:39:30 +0530

    Update DateTimeExamples.java
    Added examples on LocalTime and LocalDateTime classes

--- src/com/datesandtime/DateTimeExamples.java
+++ src/com/datesandtime/DateTimeExamples.java
@@ -1,6 +1,8 @@
 package com.datesandtime;
 
 import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.LocalDateTime;
 import java.time.Month;
 
 public class DateTimeExamples {
@@ -10,18 +12,38 @@ public class DateTimeExamples {
     }
 
     public static void main(String[] args) {
-        // LocalDate objects are immutable just like Strings
-        // So they can't be changed
-        LocalDate now = LocalDate.now();
-        p(now);
-        LocalDate hireDate = LocalDate.of(2008, Month.APRIL, 21);
-        p(hireDate);
-        LocalDate aWeekFromNow = now.plusWeeks(1); p(aWeekFromNow);
-        LocalDate aWeekAgo = now.minusWeeks(1); p(aWeekAgo);
+        /** LocalDate, LocalTime, LocalDateTime and Period objects are immutable just like Strings
+         * So they can't be changed
+         */
 
-        // But we can change the reference
-        now = now.minusWeeks(1); p(now);
+        /** LocalDate */
+//        LocalDate now = LocalDate.now();
+//        p(now);
+//        LocalDate hireDate = LocalDate.of(2008, Month.APRIL, 21);
+//        p(hireDate);
+//        LocalDate aWeekFromNow = now.plusWeeks(1); p(aWeekFromNow);
+//        LocalDate aWeekAgo = now.minusWeeks(1); p(aWeekAgo);
+//
+//        // But we can change the reference
+//        now = now.minusWeeks(1); p(now);
+//
+//        System.out.println( hireDate.isBefore(now) );
 
-        System.out.println( hireDate.isBefore(now) );
+
+        /** LocalTime */
+//        LocalTime now = LocalTime.now();
+        // Print format - Hours:Minutes:Seconds:Nano_Seconds
+//        p(now);
+//        LocalTime timeHired = LocalTime.of(13, 0, 0); p(timeHired);
+//        LocalTime anHourFromNow = now.plusHours(1); p(anHourFromNow);
+//        LocalTime anHourEarlier = now.minusHours(1); p(anHourEarlier);
+//        System.out.println( timeHired.isAfter(now) );
+
+
+        /** LocalDateTime */
+        LocalDateTime now = LocalDateTime.now(); p(now);
+        LocalDateTime timeHired = LocalDateTime.of(2008, Month.APRIL, 21, 13, 0, 0);
+        p(timeHired);
+        System.out.println( now.isBefore(timeHired) );
     }
 }
