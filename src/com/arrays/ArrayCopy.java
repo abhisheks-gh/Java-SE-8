c507a042de24b68291a3c7b7fcd759ca6ada6f63
Abhishek Kumar <95921032+abhishekkumarXD@users.noreply.github.com>
2022-09-19 15:45:18 +0530

    Added package arrays
    Added ArrayCopy.java that explains the function of arraycopy() method under System class.

---
+++ src/com/arrays/ArrayCopy.java
@@ -0,0 +1,13 @@
+package com.arrays;
+
+public class ArrayCopy {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3};
+        int[] temp = arr;
+        arr = new int[4];
+        System.arraycopy(temp, 0, arr, 0, temp.length);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
