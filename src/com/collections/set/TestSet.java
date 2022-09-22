c30ca2ee8b265403b22f0f47e1343a96cfd17a65
Abhishek Kumar <95921032+abhishekkumarXD@users.noreply.github.com>
2022-09-22 18:51:30 +0530

    Added package set

---
+++ src/com/collections/set/TestSet.java
@@ -0,0 +1,12 @@
+package com.collections.set;
+
+/** The Set interface is also a subtype (or sub-interface) of the Collection interface.
+ * Most common set implementations are:
+    -> HashSet
+    -> TreeSet */
+
+public class TestSet {
+    public static void main(String[] args) {
+
+    }
+}
